0bf700d16cc656df37b7c5a559447d58bacdfc39
Gabriel Orlando <gabriel.moraes.o@hotmail.com>
2023-06-03 02:32:32 -0300

    Playing with logging messages

--- apache-camel-ms/src/main/java/com/index325/apachecamelms/routes/MyFirstTimerRouter.java
+++ apache-camel-ms/src/main/java/com/index325/apachecamelms/routes/MyFirstTimerRouter.java
@@ -1,10 +1,49 @@
 package com.index325.apachecamelms.routes;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
+@Component
 public class MyFirstTimerRouter extends RouteBuilder {
+
+    @Autowired
+    private GetCurrentTimeBean getCurrentTimeBean;
+
+    @Autowired
+    private SimpleLoggingProcessingComponent simpleLoggingProcessingComponent;
+
     @Override
     public void configure() throws Exception {
+        from("timer:first-timer")
+                .log("${body}")
+                .transform().constant("My constant message")
+                .log("${body}")
+//                .bean("getCurrentTimeBean")
+                .bean(getCurrentTimeBean, "getCurrentTime")
+                .log("${body}")
+                .bean(simpleLoggingProcessingComponent, "process")
+                .to("log:first-timer");
+    }
+}
 
+
+@Component
+class GetCurrentTimeBean {
+    public String getCurrentTime() {
+        return "Time now is: " + LocalDateTime.now();
+    }
+}
+
+@Component
+class SimpleLoggingProcessingComponent {
+
+    private Logger logger = LoggerFactory.getLogger(SimpleLoggingProcessingComponent.class);
+    public void process(String messageBody) {
+        logger.info("SimpleLoggingProcessingComponent {}", messageBody);
     }
 }
