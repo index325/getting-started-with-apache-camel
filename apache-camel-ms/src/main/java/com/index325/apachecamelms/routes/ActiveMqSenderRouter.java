10ee43a5830c00185a603613a1945b95b088ce96
Gabriel Orlando <gabriel.moraes.o@hotmail.com>
2023-06-04 17:13:52 -0300

    Processing and transforming json file into a bean though activemq broker

--- apache-camel-ms/src/main/java/com/index325/apachecamelms/routes/ActiveMqSenderRouter.java
+++ apache-camel-ms/src/main/java/com/index325/apachecamelms/routes/ActiveMqSenderRouter.java
@@ -8,8 +8,7 @@ public class ActiveMqSenderRouter extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("timer:active-mq-timer?period=10000")
-                .transform().constant("My message for ActiveMQ")
+        from("file:files/json")
                 .log("${body}")
                 .to("activemq:my-activemq-queue");
     }
